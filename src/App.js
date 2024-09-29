9690fd8138de8362380a4f8e1c52b648f251ba76
weierliteln <1829395199@@qq.com>
2024-09-29 18:44:09 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+        < div className="bg-gray-200 w-60">
+          11
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}", // 包含你的源代码文件
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
